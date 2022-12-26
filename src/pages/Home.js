c36ab204b287804b29848a085381b485a044b2aa
akzn <40191741+akzn@users.noreply.github.com>
2022-12-26 16:50:48 +0700

    .gitignore fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import Carousell from "../components/Home/Carousell";
 import Category from "../components/Home/Category";
 import NewData from "../components/Home/New";
 import Popular from "../components/Home/Popular";
+import ReactMidtrans from "../module/react-midtrans/src/index";
 
 export default class Home extends Component {
   render() {
@@ -11,6 +12,9 @@ export default class Home extends Component {
       <>
         <Navbar />
         <Carousell />
+        <ReactMidtrans clentKey={'your-ker'} token={'8116f5e7-8a8d-4214-b5cb-21b70d5caab1'}>
+   <button> My Button For PayMe </button>
+ </ReactMidtrans>
         <Category />
         <NewData />
         <Popular />
